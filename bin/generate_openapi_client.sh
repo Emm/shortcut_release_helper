d4e164a2661b5cdc6c68cecfb3981bb2af008eb9
Emmanuel Surleau <es@impero.dk>
2022-03-26 20:19:41 +0100

    Fix pull request label data type.

--- bin/generate_openapi_client.sh
+++ bin/generate_openapi_client.sh
@@ -9,11 +9,19 @@ OUTPUT_DIR="$(dirname $0)/../shortcut_client"
 CRATE_NAME="shortcut_client"
 CRATE_VERSION="3.0.0"
 
+TEMPFILE=$(mktemp --suffix .json)
+
+curl "$SHORTCUT_OPENAPI_SPEC_URL" | \
+    jq '.definitions.PullRequestLabel.properties.id.type = "string" | del(.definitions.PullRequestLabel.properties.id.format)' \
+    > "$TEMPFILE"
+
 openapi-generator-cli generate \
-    -i "$SHORTCUT_OPENAPI_SPEC_URL" \
+    -i "$TEMPFILE" \
     -g rust \
     -p packageName="$CRATE_NAME" \
     -p packageVersion="$CRATE_VERSION" \
     -o "$OUTPUT_DIR"
+
+rm "$TEMPFILE"
 
 "$(dirname $0)/cleanup.sh"
